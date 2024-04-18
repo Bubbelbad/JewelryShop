a7f80dd4bb10b7e9779c1da8edb82a9cfca0f11e
Bubbelbad <victor.ivarson@hotmail.se>
2024-04-18 16:17:40 +0200

    New carousel on index, fixed links to other pages.

---
+++ JewelryShop/Pages/Shop/ShopPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JewelryShop.Pages.Shop
+{
+    public class ShopPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
